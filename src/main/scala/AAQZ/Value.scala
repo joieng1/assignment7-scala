e7c3e629583759348c652e085e24d81293360042
joieng1 <johncieng1@gmail.com>
2024-12-04 16:58:21 -0800

    post lab work

--- src/main/scala/AAQZ/Value.scala
+++ src/main/scala/AAQZ/Value.scala
@@ -3,9 +3,9 @@ sealed trait Value
 
 // case classes for all Value types
 final case class NumV(n : Double) extends Value
-final case class BoolV(b : BoolV) extends Value
+final case class BoolV(b : Boolean) extends Value
 final case class StrV(s : String) extends Value
-final case class CloV(params : List[Symbol], body : ExprC, env : Env) extends Value
+final case class CloV(params : Seq[Symbol], body : ExprC, env : Env) extends Value
 final case class PrimV(op : Symbol) extends Value
 
 
