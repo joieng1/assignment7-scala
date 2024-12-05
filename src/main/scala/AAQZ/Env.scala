0272698ad5bd20898113a3596b6c43eb5fd32c09
joieng1 <johncieng1@gmail.com>
2024-12-04 18:09:13 -0800

    let there be light

---
+++ src/main/scala/AAQZ/Env.scala
@@ -0,0 +1,30 @@
+package AAQZ
+
+import AAQZ.Value
+
+final case class Binding(id : Symbol, value : Value)
+
+// define env Class
+class Env(private val bindings: List[Binding] = 
+  List(Binding(Symbol("+"), new PrimV(Symbol("+"))),
+       Binding(Symbol("-"), new PrimV(Symbol("-"))),
+       Binding(Symbol("*"), new PrimV(Symbol("*"))),
+       Binding(Symbol("/"), new PrimV(Symbol("/"))),
+       Binding(Symbol("true"), new BoolV(true)),
+       Binding(Symbol("false"), new BoolV(false)),
+       Binding(Symbol("<="), new PrimV(Symbol("<="))),
+       Binding(Symbol("equal"), new PrimV(Symbol("equal"))),
+       Binding(Symbol("error"), new PrimV(Symbol("error")))
+       )) {
+    
+  // add new binding to bindings
+  def extendEnv(binding : Binding) : Env = new Env(binding :: bindings)
+
+// def lookup
+  def lookup(targetId : Symbol) : Value = {
+    bindings.find(binding => binding.id == targetId) match {
+      case Some(binding) => binding.value
+      case None => throw new NoSuchElementException(s"Binding with id $targetId cannot be found")
+    }
+  }
+}
---
+++ src/main/scala/AAQZ/Value.scala
@@ -0,0 +1,11 @@
+package AAQZ
+sealed trait Value
+
+// case classes for all Value types
+final case class NumV(n : Double) extends Value
+final case class BoolV(b : Boolean) extends Value
+final case class StrV(s : String) extends Value
+final case class CloV(params : Seq[Symbol], body : ExprC, env : Env) extends Value
+final case class PrimV(op : Symbol) extends Value
+
+
